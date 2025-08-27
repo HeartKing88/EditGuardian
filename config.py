75102be6ab4913ebd48e2a082ba00921280020b8
Vikky yadav <146602351+HeartKing88@users.noreply.github.com>
2025-08-27 15:19:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     # Bot token (from BotFather)
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "8484447768:AAGj5TwSGne1XuWV26fEEPKPnl1I0IYq-TQ")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "8342733384:AAHyqx9Zo68dv7HSGZlSlOIybv6qpc9a1QM")
 
     # Owner info
     OWNER_ID = int(os.getenv("OWNER_ID", "7338182362"))  # apna Telegram numeric user_id daalo
