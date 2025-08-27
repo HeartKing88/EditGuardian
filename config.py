1d576885d4fdca15239281e233065bab008af85c
Vikky yadav <146602351+HeartKing88@users.noreply.github.com>
2025-08-27 16:00:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,14 +2,14 @@ import os
 
 class Config:
     # Bot token (from BotFather)
-    BOT_TOKEN = os.getenv("BOT_TOKEN", None)
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 
     # Owner info
     OWNER_ID = int(os.getenv("OWNER_ID", "7548614955"))  # apna Telegram numeric user_id daalo
-    OWNER_NAME = os.getenv("OWNER_NAME", None)
+    OWNER_NAME = os.getenv("OWNER_NAME", "")
 
     # Start image (url ya local file path)
-    START_IMAGE = os.getenv("START_IMAGE", "https://files.catbox.moe/m0a0ii.jpg")  # e.g. "https://i.imgur.com/xyz.png"
+    START_IMAGE = os.getenv("START_IMAGE", "https://files.catbox.moe/aud3y6.jpg")  # e.g. "https://i.imgur.com/xyz.png"
 
     # Support group/channel
     SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/Exampurrs")
