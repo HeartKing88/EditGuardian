3d74b1575a9d82d5c7e5f05df189f4fa59dff751
Vikky yadav <146602351+HeartKing88@users.noreply.github.com>
2025-08-27 15:53:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,15 +5,15 @@ class Config:
     BOT_TOKEN = os.getenv("BOT_TOKEN", None)
 
     # Owner info
-    OWNER_ID = int(os.getenv("OWNER_ID", "7338182362"))  # apna Telegram numeric user_id daalo
-    OWNER_NAME = os.getenv("OWNER_NAME", "Bot Owner")
+    OWNER_ID = int(os.getenv("OWNER_ID", "7548614955"))  # apna Telegram numeric user_id daalo
+    OWNER_NAME = os.getenv("OWNER_NAME", "sʏsᴛᴇᴍ")
 
     # Start image (url ya local file path)
     START_IMAGE = os.getenv("START_IMAGE", "https://files.catbox.moe/m0a0ii.jpg")  # e.g. "https://i.imgur.com/xyz.png"
 
     # Support group/channel
     SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/Exampurrs")
-    SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", "https://t.me/Its_vikky_yadv")
+    SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", "https://t.me/FONT_CHANNEL_01")
 
     # Warning delete delay (seconds)
     WARNING_DELETE_DELAY = int(os.getenv("WARNING_DELETE_DELAY", "5"))
