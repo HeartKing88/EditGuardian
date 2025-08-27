5b9bc1dc849156b9973b0f4108388af5e0eec0fd
Vikky yadav <146602351+HeartKing88@users.noreply.github.com>
2025-08-27 14:33:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,11 +9,11 @@ class Config:
     OWNER_NAME = os.getenv("OWNER_NAME", "Bot Owner")
 
     # Start image (url ya local file path)
-    START_IMAGE = os.getenv("START_IMAGE", "")  # e.g. "https://i.imgur.com/xyz.png"
+    START_IMAGE = os.getenv("START_IMAGE", "https://files.catbox.moe/m0a0ii.jpg")  # e.g. "https://i.imgur.com/xyz.png"
 
     # Support group/channel
-    SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/YourSupportGroup")
-    SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", "https://t.me/YourSupportChannel")
+    SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/Exampurrs")
+    SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", "https://t.me/Its_vikky_yadv")
 
     # Warning delete delay (seconds)
     WARNING_DELETE_DELAY = int(os.getenv("WARNING_DELETE_DELAY", "5"))
